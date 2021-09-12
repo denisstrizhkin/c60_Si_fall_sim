7dcb4475b98a10f31c573e72298513dbff239417
Denis Strizhkin <strdenis02@gmail.com>
2021-09-12 10:04:09 +0300

    reading head of the dump

--- calc_scripts/c60_distrib.py
+++ calc_scripts/c60_distrib.py
@@ -9,12 +9,22 @@ AXES = {'x':2, 'y':3, 'z':4}
 
 def main():
     # read dump file
-	with open(FILE_PATH, 'r') as input_file:
-		lines = input_file.readlines()
+    with open(FILE_PATH, 'r') as input_file:
+    	lines = input_file.readlines()
+    
+    num_of_lines = len(lines)
+    num_of_atoms = int(lines[3])
+    
+    head = lines[8].split(' ')
+    head[len(head) - 1] = head[len(head) - 1][:-2]
+    keys = dict()
 
-	print(len(lines))
+    for i in range (2, len(head)):
+        keys[head[i]] = i
+   
+    print(keys)
 
-	return None
+    return None
 
 if __name__ == '__main__':
 	main()

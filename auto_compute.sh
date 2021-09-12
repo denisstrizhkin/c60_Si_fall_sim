790b21f1f96bc2856ec1cb877b7299b422948660
Denis Strizhkin <strdenis02@gmail.com>
2021-09-12 10:03:27 +0300

    added variables for c60_distrib script

--- auto_compute.sh
+++ auto_compute.sh
@@ -2,8 +2,13 @@
 
 # get script directory
 DIR=$(dirname "$0")
+SCRIPTS="$DIR/calc_scripts/"
 # string for printing stars
 STARS="******"
+# dump file name
+DUMP="all.dump"
+# zero level
+ZERO_LVL="-0.0184635"
 
 # print title
 echo "### LAMMPS AUTOCOMPUTE SCRIPT ###"; echo; echo "$STARS"
@@ -14,7 +19,7 @@ echo "set number of OpenMP threads to $OMP_NUM_THREADS"; echo; echo "$STARS"
 
 # run lammps script
 echo "running lammps script"; echo " ---"
-#lmp -sf omp -in fall_s.in
+# lmp -sf omp -in fall_s.in
 echo "$STARS"
 
-echo $(python c60_distrib.py)
+echo $(python "$SCRIPTS/c60_distrib.py" "$DIR/$DUMP" $ZERO_LVL)

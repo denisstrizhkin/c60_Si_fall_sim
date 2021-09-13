afb0b285918e8acf8160f9561bc2511703ba6685
Denis Strizhkin <strdenis02@gmail.com>
2021-09-13 22:16:59 +0300

    added c60 distribution calculation

--- auto_compute.sh
+++ auto_compute.sh
@@ -2,11 +2,12 @@
 
 # get script directory
 DIR=$(dirname "$0")
-SCRIPTS="$DIR/calc_scripts/"
+SCRIPTS="$DIR/calc_scripts"
 # string for printing stars
 STARS="******"
-# dump file name
+# dump file names
 DUMP="all.dump"
+DUMP_C_Z="c60_z.dump"
 # zero level
 ZERO_LVL="-0.0184635"
 
@@ -20,6 +21,11 @@ echo "set number of OpenMP threads to $OMP_NUM_THREADS"; echo; echo "$STARS"
 # run lammps script
 echo "running lammps script"; echo " ---"
 lmp -sf omp -in fall_s.in
-echo "$STARS"
+echo; echo "$STARS"
 
-echo $(python "$SCRIPTS/c60_distrib.py" "$DIR/$DUMP" $ZERO_LVL)
+mkdir result
+
+echo "last 10 steps C distribution average calculation"
+c60_dist=$("$SCRIPTS/c60_dist/c60_dist" "$DIR/$DUMP_C_Z")
+echo $c60_dist > "$DIR/result/c60_dist.vals"
+echo; echo "$STARS"

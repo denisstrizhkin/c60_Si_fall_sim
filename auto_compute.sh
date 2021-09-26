f03e9f710e906eb8b009c28c39e9aecdb8596528
Denis Strizhkin <strdenis02@gmail.com>
2021-09-26 20:33:44 +0300

    added for loop for different carbon positions

--- auto_compute.sh
+++ auto_compute.sh
@@ -5,6 +5,8 @@ DIR=$(dirname "$0")
 SCRIPTS="$DIR/calc_scripts"
 # string for printing stars
 STARS="******"
+# input file name
+INPUT="fall_s.input.data"
 # dump file names
 DUMP="all.dump"
 DUMP_C_Z="c60_z.dump"
@@ -19,17 +21,36 @@ echo "### LAMMPS AUTOCOMPUTE SCRIPT ###"; echo; echo "$STARS"
 export OMP_NUM_THREADS=8
 echo "set number of OpenMP threads to $OMP_NUM_THREADS"; echo; echo "$STARS"
 
-# run lammps script
-echo "running lammps script"; echo " ---"
-lmp -sf omp -in fall_s.in
-echo; echo "$STARS"
-
+# make a directory to store results
+rm -rf result
 mkdir result
 
-echo "last 10 steps C distribution average calculation"
-"$SCRIPTS/C_z_dist/C_z_dist.exe" "$DIR/$DUMP_C_Z" "$DIR/result/C_z_dist.vals"
-echo; echo "$STARS"
+# variants loop
+for i in {1..5}
+do
+  echo "compute: moved$i"; echo; echo "$STARS"
 
-echo "parsing voronoi time relation dump"
-"$SCRIPTS/Voro_time/Voro_time.exe" "$DIR/$DUMP_VOR" "$DIR/result/Voro_time.vals"
-echo; echo "$STARS"
+  RESULTS_DIR="$DIR/result/moved$i"
+  NEW_INPUT_DATA="$RESULTS_DIR/input.data"
+
+  mkdir $RESULTS_DIR
+  
+  echo "moving carbon"
+  "$SCRIPTS/C_move/C_move.exe" "$DIR/$INPUT" "$NEW_INPUT_DATA"
+  echo; echo "$STARS"
+  
+  # run lammps script
+  echo "running lammps script"; echo " ---"
+  lmp -sf omp -in $NEW_INPUT_DATA
+  echo; echo "$STARS"
+  
+  # parse carbon z distribution dump
+  echo "last 10 steps carbon distribution average calculation"
+  "$SCRIPTS/C_z_dist/C_z_dist.exe" "$DIR/$DUMP_C_Z" "$RESULTS_DIR/C_z_dist.vals"
+  echo; echo "$STARS"
+  
+  # parse voro dump
+  echo "parsing voronoi time relation dump"
+  "$SCRIPTS/Voro_time/Voro_time.exe" "$DIR/$DUMP_VOR" "$RESULTS_DIR/Voro_time.vals"
+  echo; echo "$STARS"
+done

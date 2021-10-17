1861800f78000d91c5257846c8bfa6b834bb2cd7
denis <strdenis02@gmail.com>
2021-10-17 13:28:35 +0300

    combined Dump functions and Step struct into one Dump class

--- data_parser/main.h
+++ data_parser/main.h
@@ -5,24 +5,28 @@
 #include <fstream>
 #include <map>
 
-struct Dump
+class Dump
 {
-  std::map<std::string, size_t> keys;
-  std::vector<Step> steps;
+public:
+  Dump();
+  Dump(const std::string& dump_file_path);
+
+  Dump GetSpecificStepsVals(const unsigned type,
+    const std::vector<std::string>& keys) const;
+  Dump GetAverageOfDump() const;
 
   double AtomValAt(const std::string& key,
-      const size_t step, const size_t atom);
-};
+      const size_t step, const size_t atom) const;
+  
+private:
+  struct Step
+  {
+    unsigned long time;
+    std::vector<std::vector<double>> atoms;
+  };
 
-struct Step
-{
-  unsigned long time;
-  std::vector<std::vector<double>> atoms;
+  std::map<std::string, size_t> keys;
+  std::vector<Step> steps;
 };
-
-Dump ReadDump(const std::string& dump_file_path);
-Dump GetSpecificStepsVals(const unsigned type,
-    const std::vector<std::string>& keys, const Dump& dump);
-Dump GetAverageOfDump(const Dump& dump);
 
 std::vector<std::string> SplitString(const std::string& line);

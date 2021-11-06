306a7443b2fc66ebbfed96e0063a2efd7fe32edf
Denis Strizhkin <strdenis02@gmail.com>
2021-11-07 01:43:51 +0300

    Added c60_angle_fall compute function.

--- auto_compute.sh
+++ auto_compute.sh
@@ -207,6 +207,115 @@ above_surface() {
   done
 }
 
+begin() {
+  # print title
+  echo "### LAMMPS AUTOCOMPUTE SCRIPT ###"; echo; echo "$STARS"
+  
+  # set number of OpenMP threads
+  export OMP_NUM_THREADS=8
+  echo "set number of OpenMP threads to $OMP_NUM_THREADS"; echo; echo "$STARS"
+  
+  # make a directory to store results
+  rm -rf result
+  mkdir result
+  
+  # variants loop
+  for angle_i in {0..3}
+  do
+    for move_i in {4..5}
+    do
+      COMPUTE_NAME="angle_${ANGLES[angle_i]}_move_${move_i}"
+      echo "compute: $COMPUTE_NAME"; echo; echo "$STARS"
+  
+      RESULTS_DIR="$DIR/result/$COMPUTE_NAME"
+      NEW_INPUT_DATA="$RESULTS_DIR/$INPUT"
+      NEW_IN_DATA="$RESULTS_DIR/$IN"
+  
+      mkdir $RESULTS_DIR
+      
+      echo "moving carbon"
+      $DATA_PARSER 'r' $INPUT_TEMPLATE $NEW_INPUT_DATA "temp"
+      $DATA_PARSER 'a' $NEW_INPUT_DATA $NEW_INPUT_DATA "${ANGLES[angle_i]}"
+      cp $NEW_INPUT_DATA $DIR/$INPUT
+      echo; echo "$STARS"
+  
+      echo "changing .in file"
+      echo "# CONSTANTS" > $DIR/$IN
+      echo 'variable zero_lvl equal "'$ZERO_LVL'"' >> $DIR/$IN 
+      echo 'variable carbon_vz equal "'-${Z_VELOCITIES[angle_i]}'"' >> $DIR/$IN
+      echo 'variable carbon_vx equal "'-${X_VELOCITIES[angle_i]}'"' >> $DIR/$IN
+      awk "NR >= 5" $IN_TEMPLATE >> $DIR/$IN
+      echo; echo "$STARS"
+      
+      # run lammps script
+      echo "running lammps script"; echo " ---"
+      lmp -sf omp -in fall.in
+  
+      ### COPY OUTPUT ###
+      # cp output.data
+      cp $DIR/$OUTPUT $RESULTS_DIR/$OUTPUT
+      # cp .in file
+      cp $DIR/$IN $RESULTS_DIR/$IN 
+      # cp dumps
+      cp $DIR/$DUMP_LAST_STEP $RESULTS_DIR/$DUMP_LAST_STEP
+      cp $DIR/$DUMP_LAST10 $RESULTS_DIR/$DUMP_LAST10
+      cp $DIR/$DUMP_ALL $RESULTS_DIR/$DUMP_ALL
+      cp $DIR/$DUMP_VOR $RESULTS_DIR/$DUMP_VOR
+      # cp log.lammps
+      cp $DIR/$LOG $RESULTS_DIR/$LOG
+      echo; echo "$STARS"
+      
+      # parse carbon z distribution dump
+      echo "last 10 steps carbon distribution average calculation"
+      $DATA_PARSER "c" $DIR/$DUMP_LAST10 $RESULTS_DIR/C_z_dist.vals "temp"
+      echo; echo "$STARS"
+      
+      # parse voro dump
+      echo "parsing voronoi time relation dump"
+      $DATA_PARSER "v" $DIR/$DUMP_VOR $RESULTS_DIR/Voro_time.vals "temp"
+      echo; echo "$STARS"
+
+      #clean temp files
+      clean
+    done
+  done
+}
+
+above_surface() {
+  TMP=$RESULT_DIR/above_surface.vals
+  rm -rf $TMP
+  touch $TMP
+
+  thresholds=("0" "0.2" "0.4" "0.6" "0.8" "1")
+  thresholdss=("0" "0,2" "0,4" "0,6" "0,8" "1")
+  printf "************" >> $TMP
+  for threshold_i in {0..5}
+  do
+    printf "__%1.1f" ${thresholdss[threshold_i]} >> $TMP
+  done
+  echo "" >> $TMP
+
+  for move_i in {1..5}
+  do
+    for angle_i in {0..3}
+    do
+      printf "m: %2d,a: %2d|" $move_i ${ANGLES[angle_i]} >> $TMP
+
+      COMPUTE_NAME="angle_${ANGLES[angle_i]}_move_${move_i}"
+      echo "compute: $COMPUTE_NAME"; echo; echo "$STARS"
+  
+      COMPUTE_DIR="$RESULT_DIR/$COMPUTE_NAME"
+      for threshold_i in {0..5}
+      do
+        $DATA_PARSER "u" $COMPUTE_DIR/$DUMP_LAST10 $RESULT_DIR/tmp "${thresholds[threshold_i]}"
+        tmp=$(cat $RESULT_DIR/tmp)
+        printf "%5d" $tmp >> $TMP
+      done
+      echo "" >> $TMP
+    done
+  done
+}
+
 echo "### LAMMPS AUTOCOMPUTE SCRIPT ###"; echo; echo "$stars"  
 mkdir "$results_dir"
 
